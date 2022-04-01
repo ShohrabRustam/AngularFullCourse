ea61ea789841244015c250d409dd754e0785374f
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-01 23:31:34 +0530

    show and hide data like toggle

--- src/app/task3/task3.component.ts
+++ src/app/task3/task3.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Task3Component implements OnInit {
 
+  showSecret:boolean=false;
   constructor() { }
+
 
   ngOnInit(): void {
   }
